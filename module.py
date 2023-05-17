41926dd3ec5a4e0b22ca69b79ded300933e3a84f
mendozajustin97 <mendozajustin97@gmail.com>
2023-05-16 20:08:55 -0400

    'ReUpload'

---
+++ module.py
@@ -0,0 +1,11 @@
+from math import pi
+
+
+def house_area(length, width):
+    return length*width
+
+def circumference(radius):
+    return 2*pi*radius
+
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
